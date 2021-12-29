7852d0d17c59fbe43bf03129ee7f6c20aec53c51
Ben1980 <ben.mahr@gmail.com>
2021-12-29 19:57:27 +0100

    unknown route as fallback added

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -37,6 +37,9 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (ctx) => const CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => const MealDetailScreen(),
       },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (context) => CategoryMealsScreen());
+      },
     );
   }
 }
