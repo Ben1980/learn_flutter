b25ea397bb63306f459925311834b21437796aee
Ben1980 <ben.mahr@gmail.com>
2022-01-07 14:05:15 +0100

    passing product data throug provider

--- shop_app/lib/screens/product_detail_screen.dart
+++ shop_app/lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static const routeName = '/poduct-detail';
@@ -8,10 +11,12 @@ class ProductDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
-
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
+    //listen = false because we dont need to update the details screen if a change happened to the product list
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
