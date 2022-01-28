a0c07c4f463e14d07188c751ad4c23a909108f71
Benjamin Mahr <ben.mahr@gmail.com>
2022-01-28 13:46:05 +0100

    Cart and CartItem created

---
+++ shop_app/lib/providers/cart.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    required this.id,
+    required this.title,
+    required this.quantity,
+    required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, String price, String title) {
+    if (_items.containsKey(productId)) {
+      _items[productId].quantity++;
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          quantity: quantity,
+          price: price,
+        ),
+      );
+    }
+  }
+}
