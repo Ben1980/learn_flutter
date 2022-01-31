bb75dbacb1f16931fded70b57176b0dc8c323891
Benjamin Mahr <ben.mahr@gmail.com>
2022-01-31 14:34:53 +0100

    adding items to cart

--- shop_app/lib/screens/cart_screen.dart
+++ shop_app/lib/screens/cart_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/cart.dart';
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -18,9 +19,9 @@ class CartScreen extends StatelessWidget {
       body: Column(
         children: [
           Card(
-            margin: EdgeInsets.all(15),
+            margin: const EdgeInsets.all(15),
             child: Padding(
-              padding: EdgeInsets.all(8),
+              padding: const EdgeInsets.all(8),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -47,6 +48,19 @@ class CartScreen extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (ctx, index) => CartItem(
+                  id: cart.items.values.toList()[index].id,
+                  title: cart.items.values.toList()[index].title,
+                  quantity: cart.items.values.toList()[index].quantity,
+                  price: cart.items.values.toList()[index].price),
+              itemCount: cart.itemcount,
             ),
           ),
         ],
---
+++ shop_app/lib/widgets/cart_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CartItem extends StatelessWidget {
+  final String id;
+  final double price;
+  final int quantity;
+  final String title;
+
+  const CartItem({
+    Key? key,
+    required this.id,
+    required this.price,
+    required this.quantity,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: FittedBox(
+                child: Text('\$$price'),
+              ),
+            ),
+          ),
+          title: Text(title),
+          subtitle: Text('Total: \$${price * quantity}'),
+          trailing: Text('$quantity x'),
+        ),
+      ),
+    );
+  }
+}
