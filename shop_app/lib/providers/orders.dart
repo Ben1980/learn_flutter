0297e87d972011457a405ff1dfa5b165dac3d1e0
Benjamin Mahr <ben.mahr@gmail.com>
2022-03-11 13:58:57 +0100

    Create orders.dart

---
+++ shop_app/lib/providers/orders.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+
+import './cart.dart';
+
+class OrderItem {
+  final String id;
+  final double amount;
+  final List<CartItem> products;
+  final DateTime dateTime;
+
+  OrderItem({
+    required this.id,
+    required this.amount,
+    required this.products,
+    required this.dateTime,
+  });
+}
+
+class Orders with ChangeNotifier {}
